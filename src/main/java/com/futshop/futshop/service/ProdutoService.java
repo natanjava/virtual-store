94aceae85743777a00c9858de84ce0a2eca7102a
Natan Bonfim <natan.nbg@gmail.com>
2023-11-16 12:08:41 -0300

    adjustments upload images

--- src/main/java/com/futshop/futshop/service/ProdutoService.java
+++ src/main/java/com/futshop/futshop/service/ProdutoService.java
@@ -6,6 +6,7 @@ import com.futshop.futshop.repository.ProdutoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
@@ -23,7 +25,9 @@ public class ProdutoService {
 
     @Autowired
     private ProdutoRepository produtoRepository;
-
+    
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     public List<ProdutoModel> listarProdutos(){
         return produtoRepository.findAll();
@@ -66,25 +70,26 @@ public class ProdutoService {
         return  produtoRepository.save(produto);
     }
 
-    public void salvarImagem(Long codigo, MultipartFile imagem){
-        if(! imagem.isEmpty()){
+    public void salvarImagem(Long codigo, MultipartFile imagem) {
+        if (!imagem.isEmpty()) {
             ProdutoModel produto = isProductByCode(codigo);
             String nomeImagem = imagem.getOriginalFilename();
-            produto.setImagem("uploads/"+nomeImagem);
+            produto.setImagem("uploads/" + nomeImagem);
             produtoRepository.save(produto);
-            try{
-            	 Resource resource = new ClassPathResource("static/uploads");
-                 File diretorio = resource.getFile();
-                 if (!diretorio.exists()) {
-                     diretorio.mkdirs();
-                 }
+
+            try {
+                Resource resource = new ClassPathResource("static/uploads/");
+                File diretorio = resource.getFile();
+                if (!diretorio.exists()) {
+                    diretorio.mkdirs();
+                }
 
                 File serverFile = new File(diretorio.getAbsolutePath() + File.separator + nomeImagem);
                 BufferedOutputStream stream = new BufferedOutputStream(new FileOutputStream(serverFile));
 
                 stream.write(imagem.getBytes());
                 stream.close();
-            }catch (Exception e){
+            } catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
